967d8d30c5a7d8eed318f834cfe758b5259847c4
Alex Bejenaru <alex.d.bejenaru@gmail.com>
2022-01-19 14:35:31 +0200

    finished card design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <main className="app">
+      <section className="app-container">
+        <div className="slider">
+          <div className="slider_btn"><GrFormPrevious className="icon" /></div>
+          <section className="card-container">
+            <div className="card last-slide">
+              <img className="card_image" src="https://media.istockphoto.com/photos/beautiful-afro-woman-with-perfect-makeup-picture-id1287400143?b=1&k=20&m=1287400143&s=170667a&w=0&h=3qUJYtw24sAKs73Qv-iDodDnugx4Po-RYoNAPd_pJF4=" alt="" />
+              <h3 className="card_name">Joanna</h3>
+              <div className="card_quote">
+                <img src="" className="quoteicon" alt="" />
+                <p className="quote">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas incidunt explicabo molestias vel ea minus similique totam dolore dolores quam.</p>
+              </div>
+            </div>
+            <div className="card active-slide">
+              <img className="card_image" src="https://media.istockphoto.com/photos/beautiful-afro-woman-with-perfect-makeup-picture-id1287400143?b=1&k=20&m=1287400143&s=170667a&w=0&h=3qUJYtw24sAKs73Qv-iDodDnugx4Po-RYoNAPd_pJF4=" alt="" />
+              <h3 className="card_name">Joanna</h3>
+              <div className="card_quote">
+                <img src="" className="quoteicon" alt="" />
+                <p className="quote">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas incidunt explicabo molestias vel ea minus similique totam dolore dolores quam.</p>
+              </div>
+            </div>
+            <div className="card next-slide">
+              <img className="card_image" src="https://media.istockphoto.com/photos/beautiful-afro-woman-with-perfect-makeup-picture-id1287400143?b=1&k=20&m=1287400143&s=170667a&w=0&h=3qUJYtw24sAKs73Qv-iDodDnugx4Po-RYoNAPd_pJF4=" alt="" />
+              <h3 className="card_name">Joanna</h3>
+              <div className="card_quote">
+                <img src="" className="quoteicon" alt="" />
+                <p className="quote">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas incidunt explicabo molestias vel ea minus similique totam dolore dolores quam.</p>
+              </div>
+            </div>
+          </section>
+          <div className="slider_btn"><GrFormNext className="icon" /></div>
+        </div>
+      </section>
+    </main>
   );
 }
 
