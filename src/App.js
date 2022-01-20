dc6208a5616ba580c7c64a41e99883251be9f485
Alex Bejenaru <alex.d.bejenaru@gmail.com>
2022-01-20 10:43:30 +0200

    finished project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,62 @@
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import data from './data/db';
+import { useEffect, useState } from "react";
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 
 function App() {
+  const [ people, setPeople ] = useState(data);
+  const [ index, setIndex ] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 5000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
     <main className="app">
       <section className="app-container">
         <div className="slider">
-          <div className="slider_btn"><GrFormPrevious className="icon" /></div>
+          <div className="slider_btn">
+            <GrFormPrevious onClick={() => setIndex(index - 1)} className="icon" />
+          </div>
           <section className="card-container">
-            <div className="card last-slide">
-              <img className="card_image" src="https://media.istockphoto.com/photos/beautiful-afro-woman-with-perfect-makeup-picture-id1287400143?b=1&k=20&m=1287400143&s=170667a&w=0&h=3qUJYtw24sAKs73Qv-iDodDnugx4Po-RYoNAPd_pJF4=" alt="" />
-              <h3 className="card_name">Joanna</h3>
-              <div className="card_quote">
-                <img src="" className="quoteicon" alt="" />
-                <p className="quote">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas incidunt explicabo molestias vel ea minus similique totam dolore dolores quam.</p>
+            {people.map((person, personIndex) => {
+              const { id, image, name, quote, title } = person;
+              let position = 'next-slide';
+              if (personIndex === index) {
+                position = 'active-slide';
+              }
+              if (personIndex === index - 1 || (index === 0 && personIndex === people.length -1)) {
+                position = 'last-slide';
+              }
+              return (
+              <div key={ id } className={ position + ' card'}>
+                <img className="card_image" src={ image } alt="" />
+                <h3 className="card_name">{ name }</h3>
+                <h5 className="card_title">{ title }</h5>
+                <div className="card_quote">
+                  <FaQuoteRight className="quote-icon" />
+                  <p className="quote">{ quote }</p>
+                  <FaQuoteLeft className="quote-icon quote-right" />
+                </div>
               </div>
-            </div>
-            <div className="card active-slide">
-              <img className="card_image" src="https://media.istockphoto.com/photos/beautiful-afro-woman-with-perfect-makeup-picture-id1287400143?b=1&k=20&m=1287400143&s=170667a&w=0&h=3qUJYtw24sAKs73Qv-iDodDnugx4Po-RYoNAPd_pJF4=" alt="" />
-              <h3 className="card_name">Joanna</h3>
-              <div className="card_quote">
-                <img src="" className="quoteicon" alt="" />
-                <p className="quote">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas incidunt explicabo molestias vel ea minus similique totam dolore dolores quam.</p>
-              </div>
-            </div>
-            <div className="card next-slide">
-              <img className="card_image" src="https://media.istockphoto.com/photos/beautiful-afro-woman-with-perfect-makeup-picture-id1287400143?b=1&k=20&m=1287400143&s=170667a&w=0&h=3qUJYtw24sAKs73Qv-iDodDnugx4Po-RYoNAPd_pJF4=" alt="" />
-              <h3 className="card_name">Joanna</h3>
-              <div className="card_quote">
-                <img src="" className="quoteicon" alt="" />
-                <p className="quote">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas incidunt explicabo molestias vel ea minus similique totam dolore dolores quam.</p>
-              </div>
-            </div>
+            )})}
           </section>
-          <div className="slider_btn"><GrFormNext className="icon" /></div>
+          <div className="slider_btn">
+            <GrFormNext className="icon" onClick={() => setIndex(index + 1)} />
+            </div>
         </div>
       </section>
     </main>
---
+++ src/data/db.js
@@ -0,0 +1,46 @@
+const people = [
+    {
+        id: 0,
+        name: 'Joanna',
+        title: 'Office Manager',
+        image: 'https://media.istockphoto.com/photos/beautiful-afro-woman-with-perfect-makeup-picture-id1287400143?b=1&k=20&m=1287400143&s=170667a&w=0&h=3qUJYtw24sAKs73Qv-iDodDnugx4Po-RYoNAPd_pJF4=',
+        quote: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas incidunt explicabo molestias vel ea minus similique totam dolore dolores quam.'
+    },
+    {
+        id: 1,
+        name: 'Jim',
+        title: 'Content Manager',
+        image: 'https://media.istockphoto.com/photos/portrait-young-confident-smart-asian-businessman-look-at-camera-and-picture-id1288538088?b=1&k=20&m=1288538088&s=170667a&w=0&h=3efMku7kSXUhpVrErAVVgxp6G91tRZ_5seygOn68RnE=',
+        quote: 'Aliquam a lacus elementum, tempus turpis quis, lobortis ipsum. Cras auctor imperdiet purus. Donec pharetra risus tortor, sed suscipit dui.'
+    },
+    {
+        id: 2,
+        name: 'Chris',
+        title: 'Front End Developer',
+        image: 'https://images.unsplash.com/photo-1567186937675-a5131c8a89ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGZhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        quote: 'Integer arcu ante, interdum non sagittis at, mollis id elit. Duis suscipit, massa id vehicula euismod, purus augue aliquam ipsum.'
+    },
+    {
+        id: 3,
+        name: 'Mira',
+        title: 'Front desk Assistant',
+        image: 'https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGZhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        quote: 'Nam eget urna felis. Nullam in orci laoreet, congue ligula et, dignissim nisl. Morbi eleifend pellentesque sem eget placerat. Mauris.'
+    },
+    {
+        id: 4,
+        name: 'Anna',
+        title: 'Back End Developer',
+        image: 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        quote: 'Nam ac dolor luctus, hendrerit urna a, pretium sapien. In ut varius est, vel volutpat eros. Aliquam erat volutpat. Nullam.'
+    },
+    {
+        id: 5,
+        name: 'Jason',
+        title: 'Senior Developer',
+        image: 'https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZmFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+        quote: 'Nunc diam nisl, malesuada sed lacus sit amet, efficitur feugiat arcu. Vestibulum eget felis convallis nulla fermentum lobortis. Phasellus hendrerit.'
+    }
+];
+
+export default people;
